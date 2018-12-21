0dd685e077cfa55cd5d45a99219981964cedef91
johnpoint <me@lvcshu.com>
2018-12-21 22:30:32 +0800

    Create anti-360browser.js

---
+++ anti-360browser.js
@@ -0,0 +1,2 @@
+document.write('<script src="https://raw.githubusercontent.com/mumuy/browser/master/Browser.js"><\/script>')
+if(new Browser().browser == '360' ||new Browser().browser == '360SE' ||new Browser().browser == '360SE'){window.alert('请不要使用360浏览器');document.write('<html><body bgcolor="red"><h1>请不要使用360浏览器</h1></body></html>')}
